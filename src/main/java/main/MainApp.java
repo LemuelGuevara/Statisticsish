1b45fe3b8cbfd90113714d15822ac7b844e13324
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2022-10-08 04:19:49 +0800

    Fixed fxml file directory

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -1,4 +1,8 @@
-package main.app.statisticscalculator;
+/*
+ * This class is the main class of the javaFx application
+ * */
+
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,11 +15,12 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Main.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Hello!");
+        stage.setTitle("Statistics Calculator");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {

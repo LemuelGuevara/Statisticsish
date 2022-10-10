686d00dfb32a85d7a0c31b75b4c93d2b6f4e2361
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2022-10-10 09:54:18 +0800

    Updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,11 @@ module main {
     requires org.kordamp.bootstrapfx.core;
 
     exports main;
-    exports functions;
-    exports controllers;
-    opens functions to javafx.fxml;
     opens main to javafx.fxml;
-    opens controllers to javafx.fxml;
+    exports model.statistics;
+    opens model.statistics to javafx.fxml;
+    exports controllers.text;
+    opens controllers.text to javafx.fxml;
+    exports controllers.statistics;
+    opens controllers.statistics to javafx.fxml;
 }

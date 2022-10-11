b6917c627b909396ff31b87f1cf89063a8800aad
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2022-10-12 02:07:53 +0800

    Removed unnecessary space

--- src/main/java/controllers/text/TextFieldSetter.java
+++ src/main/java/controllers/text/TextFieldSetter.java
@@ -3,7 +3,6 @@ package controllers.text;
 import javafx.scene.control.TextField;
 
 public class TextFieldSetter {
-
     public static void setTextField(TextField textField, double textFieldResults) {
         textField.setText(String.valueOf(textFieldResults));
     }
